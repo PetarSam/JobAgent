f370e2958eaf116e147d09b14f5ada35bf29e309
Product <petarsamardzija12@gmail.com>
2019-09-05 09:00:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,16 @@ import smtplib
 
 link= input('Input your link: ')
 keywords= input('Input your keywords: ').lower().split()
-email = input('Input your email: ')
+remail = input('Input your email: ')
 
-sauce = urllib.request.urlopen(link)
-soup = bs.BeautifulSoup(sauce,'lxml')
 
-body = soup.find('body').get_text().lower().split()
 
-def send_email(link,matches):
+def make_soup(link):
+    sauce = urllib.request.urlopen(link)
+    soup = bs.BeautifulSoup(sauce,'lxml')
+    body = soup.find('body').get_text().lower().split()
+
+def send_email(link, matches, remail):
     email = smtplib.SMTP('smtp.gmail.com', 587)
     email.ehlo()
     email.starttls()
@@ -22,12 +24,13 @@ def send_email(link,matches):
     subject = f'Job {matches} Found'
     body = f'Your Job Agent has found a match on the website: {link}'
     msg = f"Subject: {subject} \n\n {body}"
-    email.sendmail('jobagent.matcher@gmail.com', email, msg)
+    email.sendmail('jobagent.matcher@gmail.com', remail, msg)
     email.quit()
 
 def main():
     match=False
     matches=[]
+    make_soup(link)
     for word in body:
         if word in keywords:
             match=True

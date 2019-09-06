7a728d868a818ee44d2b187190c4a2e7b2f0bd4a
Petar <petarsamardzija12@gmail.com>
2019-09-06 12:35:23 +0200

    reconstruction

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+# from flask import Flask, render_template
 import bs4 as bs
 import urllib.request
 from time import sleep
@@ -7,11 +7,7 @@ import smtplib
 link= input('Input your link: ')
 keywords= input('Input your keywords: ').lower().split()
 remail = input('Input your email: ')
-
-def make_soup(link):
-    sauce = urllib.request.urlopen(link)
-    soup = bs.BeautifulSoup(sauce,'lxml')
-    body = soup.find('body').get_text().lower().split()
+   
 
 def send_email(link, matches, remail):
     email = smtplib.SMTP('smtp.gmail.com', 587)
@@ -26,15 +22,18 @@ def send_email(link, matches, remail):
     email.quit()
 
 def main():
+    sauce = urllib.request.urlopen(link)
+    soup = bs.BeautifulSoup(sauce,'lxml')
+    body = soup.find('body').get_text().lower().split()
     match=False
     matches=[]
-    make_soup(link)
     for word in body:
         if word in keywords:
             match=True
             matches.append(word)
     if match:
-        send_email(link, set(matches))
+        send_email(link, ' '.join(set(matches)), remail)
+        exit()
 
 while True:
     main()

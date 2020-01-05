becfcc74e42cf604afc371e853e4fbc6355f0cbe
Product <petarsamardzija12@gmail.com>
2020-01-05 17:38:52 +0100

    Code format

--- CloudFunction/index.js
+++ CloudFunction/index.js
@@ -2,11 +2,11 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = 'mongodb+srv://petarsam:'+ process.env.PASSWORD +'@cluster0-gp7st.gcp.mongodb.net/test?retryWrites=true&w=majority'
 
 exports.mongoPost = (req, res) => {
-MongoClient.connect(uri,{
-  useNewUrlParser: true,
-  useUnifiedTopology: true
- }, 
- function(err, db) {
+  MongoClient.connect(uri,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }, 
+  function(err, db) {
     if (err) throw err;
     var dbo = db.db("JobAgent");
     dbo.collection("agents").insertOne(JSON.parse(req.body), function(err, res) {
@@ -14,8 +14,7 @@ MongoClient.connect(uri,{
       res.status(200)
       db.close();
     });
-    }                                
-	);
+  });
 };
 
 
